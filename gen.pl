02302feb6ca4a3f53b89467192041486429ba9e8
Bibibye <Bibibye@users.noreply.github.com>
2015-04-26 23:15:09 +0200

    Ajout d'un flag pour limiter la récursion

--- gen.pl
+++ gen.pl
@@ -1,11 +1,11 @@
 :-['lex.pl'].
 
 % phrase
-s(s(T1,T2)) --> np(T1,N,P,_), vp(T2,N,P).
+s(s(T1,T2),Flag) --> np(T1,N,P,_), vp(T2,N,P,Flag).
 
 % syntagme nominal
 np(np(T),N,P,suj) --> prn(T,N,P).
-np(np(T1,T2),N,troisieme,comp) --> det(T1,N,G), n(T2,N,G).
+np(np(T1,T2),N,3,comp) --> det(T1,N,G), n(T2,N,G).
 n(n(T),N,G) --> nc(T,N,G).
 
 % syntagme nominal complexe
@@ -15,24 +15,24 @@ n(n(T1,T2,T3),N,G) --> adj(T1,N,G,pre), nc(T2,N,G), adj(T3,N,G,post).
 %n(N,G) --> nc(N,G), pp.
 
 % syntagme verbal
-vp(vp(T),N,P) --> v(T,N,P,n0).
-vp(vp(T1,T2),N,P) --> v(T1,N,P,n1), np(T2,_,_,comp).
-vp(vp(T1,T2),N,P) --> v(T1,N,P,comp), comp(T2).
-vp(vp(T1,T2),N,P) --> v(T1,N,P,vcomp), vcomp(T2).
+vp(vp(T),N,P,_) --> v(T,N,P,n0).
+vp(vp(T1,T2),N,P,_) --> v(T1,N,P,n1), np(T2,_,_,comp).
+vp(vp(T1,T2),N,P,1) --> v(T1,N,P,comp), comp(T2,0).
+vp(vp(T1,T2),N,P,Flag) --> v(T1,N,P,vcomp), vcomp(T2,Flag).
 % vp(N,P) --> v(N,P), np(_,_,comp), pp.
 %vp(N,P) --> v(N,P), pp, comp.
 
 % infinitif
-vcomp(vcomp(T)) --> vinf(T,n0).
-vcomp(vcomp(T1,T2)) --> vinf(T1,n1), np(T2,_,_,comp).
-vcomp(vcomp(T1,T2)) --> vinf(T1,comp), comp(T2).
-vcomp(vcomp(T1,T2)) --> vinf(T1,vcomp), vcomp(T2).
+vcomp(vcomp(T),_) --> vinf(T,n0).
+vcomp(vcomp(T1,T2),_) --> vinf(T1,n1), np(T2,_,_,comp).
+vcomp(vcomp(T1,T2),1) --> vinf(T1,comp), comp(T2,0).
+vcomp(vcomp(T1,T2),1) --> vinf(T1,vcomp), vcomp(T2,0).
 %vcomp --> vinf, np(_,_,comp), pp.
 %vcomp --> vinf, pp, comp.
 
 
 % subordonnee completive
-comp(comp(T1,T2)) --> cs(T1), s(T2).
+comp(comp(T1,T2), Flag) --> cs(T1), s(T2,Flag).
 
 % groupe prepositionnel
 pp(pp(T1,T2)) --> prep(T1), np(T2,_,_,comp).
