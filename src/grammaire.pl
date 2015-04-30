63e69afee3bb41c2fcca7d6b69de9fd135d7afe4
Bibibye <Bibibye@users.noreply.github.com>
2015-04-30 18:12:26 +0200

    Correction commentaires

--- src/grammaire.pl
+++ src/grammaire.pl
@@ -1,21 +1,21 @@
 :-['lexique.pl'].
 
-% phrase
+%% phrase
 s(s(T1,T2),Enchassement,_,D) --> np(T1,N,P,_,D,F), vp(T2,N,P,F,Enchassement).
 % s(s(T1,T2,T3),Enchassement,1,D) --> s(T1,Enchassement,0,D), cc(T2,F), s(T3,Enchassement,0,F).
 
-% syntagme nominal
+%% syntagme nominal
 np(np(T),N,P,suj,D,F) --> prn(T,N,P,D,F).
 np(np(T1,T2),N,3,comp,D,con) --> det(T1,N,G,D,F), n(T2,N,G,F).
 n(n(T),N,G,D) --> nc(T,N,G,D).
 
-% syntagme nominal complexe
+%% syntagme nominal complexe
 n(n(T1,T2),N,G,D) --> adj(T1,N,G,pre,D), nc(T2,N,G,_).
 n(n(T1,T2),N,G,D) --> nc(T1,N,G,D), adj(T2,N,G,post,_).
 n(n(T1,T2,T3),N,G,D) --> adj(T1,N,G,pre,D), nc(T2,N,G,_), adj(T3,N,G,post,_).
 % n(N,G) --> nc(N,G), pp.
 
-% syntagme verbal
+%% syntagme verbal
 vp(vp(T),N,P,D,_) --> v(T,N,P,n0,D).
 vp(vp(T1,T2),N,P,D,_) --> v(T1,N,P,n1,D), np(T2,_,_,comp,_,_).
 vp(vp(T1,T2),N,P,D,1) --> v(T1,N,P,comp,D), comp(T2,0).
@@ -23,7 +23,7 @@ vp(vp(T1,T2),N,P,D,Enchassement) --> v(T1,N,P,vcomp,D), vcomp(T2,_,Enchassement)
 % vp(N,P) --> v(N,P), np(_,_,comp), pp.
 % vp(N,P) --> v(N,P), pp, comp.
 
-% infinitif
+%% infinitif
 vcomp(vcomp(T),D,_) --> vinf(T,n0,D).
 vcomp(vcomp(T1,T2),D,_) --> vinf(T1,n1,D), np(T2,_,_,comp,_,_).
 vcomp(vcomp(T1,T2),D,1) --> vinf(T1,comp,D), comp(T2,0).
@@ -31,13 +31,13 @@ vcomp(vcomp(T1,T2),D,1) --> vinf(T1,vcomp,D), vcomp(T2,_,0).
 % vcomp --> vinf, np(_,_,comp), pp.
 % vcomp --> vinf, pp, comp.
 
-% subordonnee completive
+%% subordonnee completive
 comp(comp(T1,T2),Enchassement) --> cs(T1,F), s(T2,Enchassement,0,F).
 
-% groupe prepositionnel
+%% groupe prepositionnel
 pp(pp(T1,T2)) --> prep(T1,F), np(T2,_,_,comp,F).
 
-% terminaux
+%% terminaux
 v(v(Mot),N,P,V,D) --> [Mot], {est(Mot, v),
 			      accord(Mot, N, _, P),
 			      valence(Mot, V),
--- src/lexique.pl
+++ src/lexique.pl
@@ -1,34 +1,34 @@
-% v vinf cs prep adj nc prn det
+%% v vinf cs prep adj nc prn det
 est(Mot,Nature) :- lex(Mot,Nature,_,_,_,_,_,_,_).
 
-% sing plur
+%% sing plur
 nombre(Mot,Nombre) :- lex(Mot,_,Nombre,_,_,_,_,_,_).
 
-% masc fem
+%% masc fem
 genre(Mot,Genre) :- lex(Mot,_,_,Genre,_,_,_,_,_).
 
-% 1 2 3
+%% 1 2 3
 personne(Mot,Personne) :- lex(Mot,_,_,_,Personne,_,_,_,_).
 
 %%
 accord(Mot,Nombre,Genre,Personne) :- lex(Mot,_,Nombre,Genre,Personne,_,_,_,_).
 
-% n0 n1 aN1 deN1 comp vcomp aN1comp
+%% n0 n1 aN1 deN1 comp vcomp aN1comp
 valence(Mot,Valence) :- lex(Mot,_,_,_,_,Valence,_,_,_).
 
-% pre post both
+%% pre post both
 position(Mot,Position) :- lex(Mot,_,_,_,_,_,Position,_,_).
 
-% voy con
+%% voy con
 debut(Mot,Debut) :- lex(Mot,_,_,_,_,_,_,Debut,_).
 
-% con voy
+%% con voy
 forme(Mot,Forme) :- lex(Mot,_,_,_,_,_,_,_,Forme).
 
-% lex(mot,nature,nombre,genre,personne,valence,position,debut,forme)
+%% lex(mot,nature,nombre,genre,personne,valence,position,debut,forme)
 
 
-				% déterminants %
+%% déterminants
 
 lex('le',det,sing,masc,_,_,_,con,con).
 lex('un',det,sing,masc,_,_,_,voy,_).
@@ -39,7 +39,7 @@ lex('l',det,sing,_,_,_,_,con,voy).
 % lex('des',det,plur,_,_,_,_,con,_).
 
 
-				% noms communs %
+%% noms communs
 
 % lex('porte',nc,sing,fem,_,_,_,con,_).
 lex('gnou',nc,sing,masc,_,_,_,con,_).
@@ -49,7 +49,7 @@ lex('maroufle',nc,sing,fem,_,_,_,con,_).
 % lex('portes',nc,plur,fem,_,_,_,con,_).
 
 
-				% verbes conjugués %
+%% verbes conjugués
 
 lex('mange',v,sing,_,1,n1,_,con,_).
 lex('manges',v,sing,_,2,n1,_,con,_).
@@ -87,7 +87,7 @@ lex('dit',v,sing,_,3,comp,_,con,_).
 % lex('disent',v,plur,_,3,comp,_,con,_).
 
 
-				% verbes infinitif %
+%% verbes infinitif
 
 lex('manger',vinf,_,_,_,n1,_,con,_).
 lex('marcher',vinf,_,_,_,n0,_,con,_).
@@ -95,20 +95,20 @@ lex('marcher',vinf,_,_,_,n0,_,con,_).
 lex('dire',vinf,_,_,_,comp,_,con,_).
 
 
-				% conjonctions de subordination %
+%% conjonctions de subordination
 
 lex('que',cs,_,_,_,_,_,con,con).
 lex('qu',cs,_,_,_,_,_,con,voy).
 
 
-				% prépositions %
+%% prépositions
 
 % lex('de',prep,_,_,_,_,_,con,con).
 % lex('d',prep,_,_,_,_,_,con,voy).
 % lex('à',prep,_,_,_,_,_,con,_).
 
 
-				% conjonctions de coordination %
+%% conjonctions de coordination
 
 % lex('mais',cc,_,_,_,_,_,con,_).
 % lex('ou',cc,_,_,_,_,_,con,_).
@@ -120,7 +120,7 @@ lex('qu',cs,_,_,_,_,_,con,voy).
 % lex('tandis qu',cc,_,_,_,_,_,con,voy).
 
 
-				% adjectifs qualificatifs %
+%% adjectifs qualificatifs
 
 lex('rouge',adj,sing,_,_,_,post,con,_).
 % lex('rouges',adj,plur,_,_,_,post,con,_).
@@ -136,7 +136,7 @@ lex('poilue',adj,sing,fem,_,_,post,con,_).
 % lex('poilues',adj,plur,fem,_,_,post,con,_).
 
 
-				% pronoms personnels sujets %
+%% pronoms personnels sujets
 
 lex('je',prn,sing,_,1,_,_,con,con).
 lex('j',prn,sing,_,1,_,_,con,voy).
